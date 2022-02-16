dd327f489bc75f744d594105c7b469c645a71ac7
rbnfrhnr <frehner.robin@gmail.com>
2022-02-16 06:55:18 -0800

    Cuda Support

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ WORKDIR ./anomaly-detection
 EXPOSE 8745
 RUN apt update -y
 RUN apt upgrade -y
+RUN apt install wget -y
 RUN apt-get install -y python3.8 \
     && ln -s /usr/bin/python3.8 /usr/bin/python3
 RUN apt install tzdata -y
@@ -14,6 +15,6 @@ RUN apt install nvidia-cuda-toolkit -y
 RUN ./cuda-setup.sh
 RUN apt install python3-pip -y
 RUN pip3 install -r ./requirements.txt
-RUN wandb login
+RUN wandb login 19baab9756788cf65a408e362a628caa03d6c3c1
 RUN which python3
 
--- server-setup.sh
+++ server-setup.sh
@@ -1,10 +1,10 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update
-sudo apt-get install libcudnn8=8.2.4.15-1+cuda11.4
-sudo apt-get install libcudnn8-dev=8.2.4.15-1+cuda11.4
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+apt-get update
+apt-get install libcudnn8=8.2.4.15-1+cuda11.4
+apt-get install libcudnn8-dev=8.2.4.15-1+cuda11.4
 
 
 
