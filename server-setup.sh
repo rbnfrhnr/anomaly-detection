250cb62b4412067ad2066193205becf9c8bb1b89
rbnfrhnr <frehner.robin@gmail.com>
2022-02-17 02:47:52 -0800

    server setup script

--- server-setup.sh
+++ server-setup.sh
@@ -1,10 +1,4 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-apt-get update
-apt-get install libcudnn8=8.2.4.15-1+cuda11.4
-apt-get install libcudnn8-dev=8.2.4.15-1+cuda11.4
+./cuda-setup.sh
 
 
 
