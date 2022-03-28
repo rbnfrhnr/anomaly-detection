b15a0e5c40fda487875dcb00b7df4dd087a9b850
rbnfrhnr <frehner.robin@gmail.com>
2022-03-28 13:31:30 -0700

    code cleanup

--- evaluation/mit_bih_evaluator.py
+++ evaluation/mit_bih_evaluator.py
@@ -21,7 +21,7 @@ def p_at_k(vae, downstream_model, task, **config):
     mit_set = load_normal(config['data']['location'], nr=803)[0]
     eval_data = MinMaxScaler().fit_transform(mit_set.reshape(-1, 1))
     nr_sequences = math.floor(eval_data.shape[0] / window_size)
-    eval_idxs = np.arange(eval_data.shape[0]).reshape(nr_sequences, window_size, 1)
+    eval_idxs = np.arange(nr_sequences * window_size).reshape(nr_sequences, window_size, 1)
     eval_data = eval_data[:window_size * nr_sequences]
     eval_data = eval_data.reshape(nr_sequences, window_size, 1)
     mal_idxs = np.array(anot.sample)[(np.array(anot.symbol) != 'N').nonzero()[0]]
@@ -50,41 +50,39 @@ def p_at_k(vae, downstream_model, task, **config):
     logger.info('', extra={'data': {'context': 'eval', 'entity': 'scencario-803', 'item': 'raw-data', 'data': log}})
 
 
-def eval(test_data, vae, downstream_model, scenario, task, **config):
-    test_norm, test_bot = test_data
+def eval_all(test_data, vae, downstream_model, scenario, task, **config):
     logger = utils.get_logger(**config)
     context = 'eval-' + task
     entity = 'scenario-' + scenario
-    p_at_k(vae, downstream_model, task, **config)
+    window_size = config['preprocessing']['time-steps']
     mit_set = load_normal(config['data']['location'], nr=803)[0]
     eval_data = MinMaxScaler().fit_transform(mit_set.reshape(-1, 1))
-    window_size = test_norm.shape[1]
-    y = np.concatenate((np.zeros(test_norm.shape[0]), np.ones(test_bot.shape[0])), axis=None)
+    test_data = eval_data[0:math.floor(eval_data.shape[0] / window_size) * window_size]
+    test_data = test_data.reshape(-1, window_size, 1)
 
-    # indexer = np.arange(window_size)[None, :] + 1 * np.arange(eval_data.shape[0] - window_size - 1)[:, None]
-    # sl_windows = eval_data[indexer]
-    sl_windows = eval_data.reshape(-1, window_size, 1)
-    score_norm, score_mal, recon_err = utils.predict2(vae, downstream_model, sl_windows, axis=(1, 2))
+    score_norm, score_mal, recon_err = utils.predict2(vae, downstream_model, test_data, axis=(1, 2))
     preds = score_mal > score_norm
-    mal_idx = (score_mal > score_norm).nonzero()[0]
 
     start = round(12 * 128)
     end = round(18 * 128)
     min_in_slice = np.min(mit_set[window_size: eval_data.shape[0]][start:end])
     max_in_slice = np.max(mit_set[window_size: eval_data.shape[0]][start:end])
-    fill_int = mal_idx[np.isin(mal_idx, np.arange(start, end))]
-    fill_int = np.split(fill_int, np.where(np.diff(fill_int) != 1)[0] + 1)
+
+    # fill_int = np.split(fill_int, np.where(np.diff(fill_int) != 1)[0] + 1)
+    score_norm_rep = np.repeat(score_norm, window_size)
+    score_mal_rep = np.repeat(score_mal, window_size)
+    preds_rep = np.repeat(preds, window_size)
+    xx = np.arange(eval_data.shape[0])
+    fill = xx[start:end][np.isin(xx[start:end], preds_rep.nonzero()).nonzero()]
+    fill_idx = np.split(fill, np.where(np.diff(fill) != 1)[0] + 1)
+
     _, ax = plt.subplots(2, 1, figsize=(17, 5))
-    ax[0].plot(list(range(0, eval_data.shape[0]))[start:end], mit_set[window_size: eval_data.shape[0]][start:end],
-               label='ecg')
-    for x in fill_int:
-        ax[0].fill_between(x, min_in_slice, max_in_slice, alpha=0.2, color='red')
-    ax[1].plot(list(range(0, eval_data.shape[0] - window_size - 1))[start:end], score_norm[start:end], color='green',
-               label='KDE value - normal')
-    ax[1].plot(list(range(0, eval_data.shape[0] - window_size - 1))[start:end], score_mal[start:end], color='red',
-               label='KDE value - anomaly')
-    ax[1].plot(list(range(0, eval_data.shape[0] - window_size - 1))[start:end], preds[start:end], color='blue',
-               label='is anomaly')
+    ax[0].plot(xx[start:end], mit_set[window_size: eval_data.shape[0]][start:end], label='ecg')
+    for x in fill_idx:
+        ax[0].fill_between(x, -1, 2, alpha=0.2, color='red')
+    ax[1].step(xx[start:end], score_norm_rep[start:end], color='green', label='KDE value - normal')
+    ax[1].step(xx[start:end], score_mal_rep[start:end], color='red', label='KDE value - anomaly')
+    ax[1].step(xx[start:end], preds_rep[start:end], color='blue', label='is anomaly')
     ax[0].set_xlabel('frame')
     ax[0].set_ylabel('mm')
     ax[1].set_xlabel('frame')
@@ -100,6 +98,18 @@ def eval(test_data, vae, downstream_model, scenario, task, **config):
                        data=np.stack([recon_err, score_norm, score_mal], axis=1))
     sl_item = 'sliding-windows-scores'
     logger.info('', extra={'data': {'context': context, 'entity': entity, 'item': sl_item, 'data': log}})
+
+
+def eval(test_data, vae, downstream_model, scenario, task, **config):
+    test_norm, test_bot = test_data
+    logger = utils.get_logger(**config)
+    context = 'eval-' + task
+    entity = 'scenario-' + scenario
+    eval_all(test_data, vae, downstream_model, scenario, task, **config)
+    p_at_k(vae, downstream_model, task, **config)
+    mit_set = load_normal(config['data']['location'], nr=803)[0]
+    eval_data = MinMaxScaler().fit_transform(mit_set.reshape(-1, 1))
+    window_size = test_norm.shape[1]
 
     test_d = np.concatenate([test_norm, test_bot], axis=0)
     y = np.concatenate((np.zeros(test_norm.shape[0]), np.ones(test_bot.shape[0])), axis=None)
