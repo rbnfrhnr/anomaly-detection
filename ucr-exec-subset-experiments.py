7d57f4029b536c757ed465c4d11d9b8100fd89f6
rbnfrhnr <frehner.robin@gmail.com>
2022-05-12 05:27:38 -0700

    augmentation name fixes

--- ucr-exec-subset-experiments.py
+++ ucr-exec-subset-experiments.py
@@ -41,18 +41,20 @@ if __name__ == '__main__':
             with open(run_file_name, 'w+') as yaml_file:
                 yaml.dump(cfg, yaml_file, default_flow_style=False)
 
-    # for i in range(1, 101):
-    #     for file in data_files:
-    #         file_name = file.split('/')[-1]
-    #         set_nr = file_name.split('_')[0]
-    #         cfg['data']['train-sets'] = [set_nr]
-    #         cfg['experiment-name'] = expriment_name_template + '-' + set_nr
-    #
-    #         run_template_location = '/'.join(template.split('/')[0:-1]) + '/run-templates'
-    #         runt_template_name = template.split('/')[-1].split('.')[0] + '-' + set_nr + '.yaml'
-    #         run_file_name = run_template_location + '/' + runt_template_name
-    #         os.makedirs(os.path.dirname(run_file_name), exist_ok=True)
-    #         with open(run_file_name, 'w+') as yaml_file:
-    #             yaml.dump(cfg, yaml_file, default_flow_style=False)
-    #
-    #         os.system('python3 vae_rnn.py ' + run_file_name)
+            os.system('python3 vae_rnn.py ' + run_file_name)
+
+# for i in range(1, 101):
+#     for file in data_files:
+#         file_name = file.split('/')[-1]
+#         set_nr = file_name.split('_')[0]
+#         cfg['data']['train-sets'] = [set_nr]
+#         cfg['experiment-name'] = expriment_name_template + '-' + set_nr
+#
+#         run_template_location = '/'.join(template.split('/')[0:-1]) + '/run-templates'
+#         runt_template_name = template.split('/')[-1].split('.')[0] + '-' + set_nr + '.yaml'
+#         run_file_name = run_template_location + '/' + runt_template_name
+#         os.makedirs(os.path.dirname(run_file_name), exist_ok=True)
+#         with open(run_file_name, 'w+') as yaml_file:
+#             yaml.dump(cfg, yaml_file, default_flow_style=False)
+#
+#         os.system('python3 vae_rnn.py ' + run_file_name)
