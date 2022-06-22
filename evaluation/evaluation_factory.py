ce1bfb14740fbce032e39ec1a0ccdba92fbe95dd
rbnfrhnr <frehner.robin@gmail.com>
2022-06-22 14:21:09 -0700

    time window smoothing

--- evaluation/evaluation_factory.py
+++ evaluation/evaluation_factory.py
@@ -1,11 +1,11 @@
 from evaluation.default_evaluator import test_eval
 from evaluation.mit_bih_evaluator import eval
-from evaluation.ucr_evaluator import evaluate
+from evaluation.ucr_evaluator import evaluate, evaluate2
 
 def get_evaluator(dataset):
     if dataset == 'mit-bih':
         return eval
     if dataset == 'ucr':
-        return evaluate
+        return evaluate2
     else:
         return test_eval
